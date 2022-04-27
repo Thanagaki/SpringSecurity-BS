93c29b7c07a21f6f328c46a7da3085cffb85729c
Thanagaki <100522683+Thanagaki@users.noreply.github.com>
2022-04-27 19:09:26 +0300

    Доделать

---
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/user").setViewName("user");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/").setViewName("index");
+
+
+    }
+}
+
